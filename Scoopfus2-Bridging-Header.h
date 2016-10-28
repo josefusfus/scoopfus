8afe4c6bff415c828be9f18c9166ed3f62644d65
José Luís Sáez Sánchez <joseluissaez@me.com>
2016-10-28 22:20:07 +0200

    Commit inicial,aplicacion preparada con FrameWork listo

---
+++ Scoopfus2-Bridging-Header.h
@@ -0,0 +1,9 @@
+//
+//  Scoopfus2-Bridging-Header.h
+//  scoopfus2
+//
+//  Created by jose luis saez sanchez on 28/10/16.
+//  Copyright © 2016 JoseFusFus. All rights reserved.
+//
+
+#import <MicrosoftAzureMobile/MicrosoftAzureMobile.h>
